cbf4a13f7b6186361057630ef2814a13c8c26ecd
Tram Le <lenubaotram2606@gmail.com>
2025-10-28 01:42:11 -0400

    replace the API code

--- weather app/script.js
+++ weather app/script.js
@@ -1,70 +1,62 @@
-function fetchWeather() {
-    let searchInput = document.getElementById("search").ariaValueMax;
-    const weatherDataSection = document.getElementById("weather-data");
-    const apiKey = "REPLACE WITH YOUR API KEY";
-}
+async function fetchWeather() {
+  let searchInput = document.getElementById('search').value;
+  const weatherDataSection = document.getElementById("weather-data");
+  weatherDataSection.style.display = "block";
 
-function fetchWeather() {
-    if (searchInput == "") {
+  const apiKey = "513bba0e94ff83decd3c81cf0a7652ff"
+
+  if (searchInput == "") {
     weatherDataSection.innerHTML = `
     <div>
-        <h2>Empty Input!</h2>
-        <p>Please try again with a valid <u>city name</u>.</p>
+      <h2>Empty Input!</h2>
+      <p>Please try again with a valid <u>city name</u>.</p>
     </div>
     `;
     return;
+  }
+  async function getLonAndLat() {
+    const countryCode = 1
+    const geocodeURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}`
+
+    const response = await fetch(geocodeURL);
+    if (!response.ok) {
+      console.log("Bad response! ", response.status);
+      return;
     }
-    async function getLonAndLat(){
-        const countryCode = 1;
-        const geoCodingUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`;
-        const response = await fetch(geoCodeUrl);
-        if (!response.ok) {
-            console.log("Bad response! ", response.status);
-            return;
-        }
-        const data = await response.json();
-        if (data.length == 0) {
-            console.log("Something went wrong here.");
-            weatherDataSection.innerHTML =`
-            <div>
-                <h2>Invalid Input: "${searchInput}"</h2>
-                <p>Please try again with a valid <u>city name</u>.</p>
-            </div>
-            `;
-            return;
-        } else {
-            return data[0];
-        }
+
+    const data = await response.json();
+    if (data.length == 0) {
+      console.log("Something went wrong here.");
+      weatherDataSection.innerHTML = `
+      <div>
+        <h2>Invalid Input: "${searchInput}"</h2>
+        <p>Please try again with a valid <u>city name</u>.</p>
+      </div>
+      `;
+      return;
+    } else {
+      return data[0];
     }
-    async function getWeatherData(lon, lat) {
-        const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-        const response = await fetch(weatherURL);
-        if (!response.ok) {
-            console.log("Bad response! ", response.status);
-            return;
-        }
-        const data = await response.json()
-        weatherDataSection.innerHTML = `
-    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" />
-    <div>
-    <h2>${data.name}</h2>
-    <p><strong>Temperature:</strong> ${Math.round(data.main.temp - 273.15)}°C</p>
-    <p><strong>Description:</strong> ${data.weather[0].description}</p>
-    </div>
-    `
+  }
+
+  async function getWeatherData(lon, lat) {
+    // Step c. Get weather information via Current Weather API
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    const response = await fetch(weatherURL);
+
+    // Step d. Display the weather data
+    const data = await response.json();
     weatherDataSection.style.display = "flex";
     weatherDataSection.innerHTML = `
-    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" />
-    <div>
+      <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" />
+      <div>
         <h2>${data.name}</h2>
         <p><strong>Temperature:</strong> ${Math.round(data.main.temp - 273.15)}°C</p>
         <p><strong>Description:</strong> ${data.weather[0].description}</p>
-    </div>
+      </div>
     `
-    }
-    async function fetchWeather() {
-        document.getElementById("search").value = "";
-        const geocodeData = await getLonAndLat();
-        getWeatherData(geocodeData.lon, geocodeData.lat);
-    }
+  }
+  document.getElementById("search").value = "";
+  const geocodeData = await getLonAndLat();
+  getWeatherData(geocodeData.lon, geocodeData.lat);
 }
