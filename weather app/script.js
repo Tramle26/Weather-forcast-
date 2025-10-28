a316483b777b3ee5a810b1b188b75ae19ff34bc2
Tram Le <lenubaotram2606@gmail.com>
2025-10-27 22:44:13 -0400

    add fetchweather() fix the visual of web in css

---
+++ weather app/script.js
@@ -0,0 +1,5 @@
+function fetchWeather() {
+    let searchInput = document.getElementById("search").ariaValueMax;
+    const weatherDataSection = document.getElementById("weather-data");
+    const apiKey = "REPLACE WITH YOUR API KEY";
+}
